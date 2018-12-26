368f85afbd27f61318e20006f489747e4028adac
ipe <p.ivanov@skywell.com.ua>
2018-12-26 15:09:56 +0200

    EH-53:fix styles and merging into staging

--- app/common/components/SortableHandle/index.js
+++ app/common/components/SortableHandle/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { SortableHandle } from "react-sortable-hoc";
 import withStyles from "nebo15-isomorphic-style-loader/lib/withStyles";
-import styles from "../Input/styles.scss";
+import styles from "./styles.scss";
 
-const Handle = SortableHandle(({title}) => (
-  <div className="handle" title={title}>Handle</div>
+const Handle = SortableHandle(({ title }) => (
+  <div className={styles.handle} title={title}>
+    <div/>
+    <div/>
+    <div/>
+  </div>
 ));
 
 export default withStyles(styles)(Handle);
