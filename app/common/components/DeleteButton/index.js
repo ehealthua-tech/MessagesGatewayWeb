ce5a8c8b40838a6f5b2843ff69d13104586953da
ipe <p.ivanov@skywell.com.ua>
2019-01-09 16:39:45 +0200

    EH-53:adds delete button

--- app/common/components/DeleteButton/index.js
+++ app/common/components/DeleteButton/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import withStyles from "nebo15-isomorphic-style-loader/lib/withStyles";
 import styles from "./styles.scss";
+import Icon from "../Icon";
 
 const DeleteButton = ({ onClick }) => (
   <div onClick={onClick} className={styles.delete_button}>
-    <div />
-    <div />
+    <Icon name="trash" />
   </div>
 );
 
